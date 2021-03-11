c66f42c07a13c8c346db392e4dc312c73b2c4d58
Caike Colantonio <caikecolantonio@Caikes-Mac-mini.local>
2021-03-11 00:24:11 -0300

    Presentation

---
+++ components/CarItem/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+  carContainer: {
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").height,
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subtittleCTA: {
+    textDecorationLine: "underline",
+  },
+  subtittle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 50,
+    width: "100%",
+  },
+});
+
+export default styles;
